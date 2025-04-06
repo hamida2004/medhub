ee96dbf7fb5cd4f8c7f73206da9412f569617341
unknown <hamida2004dadda@gmail.com>
2025-04-06 03:15:13 +0200

    updated

--- src/assets/links.js
+++ src/assets/links.js
@@ -5,8 +5,8 @@ export const specialities = [
     years: [
       {
         id: 1, year: "1st year", drives: [
-          { id: 1, faculty: "Bechar", driveLink: "https://drive.google.com/drive/folders/1-0G8KYEe_UD7i2OKq6jIQVpSsQmELaWu", year: "2024-2025" }
-          , { id: 2, faculty: "Bechar", driveLink: "https://drive.google.com/drive/folders/1ghB553nHWEuD8c9gvK_fJXo8d1xttt87", year: "2023-2024" }
+          { id: 1, faculty: "Bechar", driveLink: "https://drive.google.com/drive/folders/1mUHPW6PJe80Qncpi4uJsVoxx5J_U5qos", year: "2024-2025" }
+          , { id: 2, faculty: "Bechar", driveLink: "https://drive.google.com/drive/folders/1bdVFHj_aXF80VNUFvDJ1uwL5-QMplmPG", year: "2023-2024" }
         ]
       },
       {
@@ -18,39 +18,47 @@ export const specialities = [
       },
       {
         id: 3, year: "3rd year",
-        drives: [{
-          id: 3, faculty: "Bechar",
-          driveLink: "https://drive.google.com/drive/folders/1yHYm2CziVcr5rYkMLeFyCDBInyKGfNhg", year: "2024-2025"
-        }]
+        drives: []
       },
-      { id: 4, year: "4th year", drives: [{ id: 4, faculty: "Blida", driveLink: "https://drive.google.com/drive/folders/1-0G8KYEe_UD7i2OKq6jIQVpSsQmELaWu", year: "2024-2025" }] },
-      { id: 5, year: "5th year", drives: [{ id: 5, faculty: "Annaba", driveLink: "https://drive.google.com/drive/folders/1-0G8KYEe_UD7i2OKq6jIQVpSsQmELaWu", year: "2024-2025" }] },
-      { id: 6, year: "6th year", drives: [{ id: 6, faculty: "Tizi Ouzou", driveLink: "https://drive.google.com/drive/folders/1-0G8KYEe_UD7i2OKq6jIQVpSsQmELaWu", year: "2024-2025" }] },
-      { id: 7, year: "7th year", drives: [{ id: 7, faculty: "Sétif", driveLink: "https://drive.google.com/drive/folders/1-0G8KYEe_UD7i2OKq6jIQVpSsQmELaWu", year: "2024-2025" }] }
+      { id: 4, year: "4th year", drives: [] },
+      { id: 5, year: "5th year", drives: [] },
+      { id: 6, year: "6th year", drives: [] },
+      { id: 7, year: "7th year", drives: [] }
     ]
   },
   {
     id: 2,
     name: "pharma",
     years: [
-      { id: 1, year: "1st year", drives: [{ id: 1, faculty: "Sidi Bel Abess", driveLink: "https://drive.google.com/drive/folders/1-0G8KYEe_UD7i2OKq6jIQVpSsQmELaWu", year: "2024-2025" }] },
-      { id: 2, year: "2nd year", drives: [{ id: 2, faculty: "Setif", driveLink: "https://drive.google.com/drive/folders/1-0G8KYEe_UD7i2OKq6jIQVpSsQmELaWu", year: "2024-2025" }] },
-      { id: 3, year: "3rd year", drives: [{ id: 3, faculty: "Algiers", driveLink: "https://drive.google.com/drive/folders/1-0G8KYEe_UD7i2OKq6jIQVpSsQmELaWu", year: "2024-2025" }] },
-      { id: 4, year: "4th year", drives: [{ id: 4, faculty: "Oran", driveLink: "https://drive.google.com/drive/folders/1-0G8KYEe_UD7i2OKq6jIQVpSsQmELaWu", year: "2024-2025" }] },
-      { id: 5, year: "5th year", drives: [{ id: 5, faculty: "Constantine", driveLink: "https://drive.google.com/drive/folders/1-0G8KYEe_UD7i2OKq6jIQVpSsQmELaWu", year: "2024-2025" }] },
-      { id: 6, year: "6th year", drives: [{ id: 6, faculty: "Blida", driveLink: "https://drive.google.com/drive/folders/1-0G8KYEe_UD7i2OKq6jIQVpSsQmELaWu", year: "2024-2025" }] }
+      {
+        id: 1, year: "1st year", drives: [{ id: 1, faculty: "Sidi Bel Abess", driveLink: "https://drive.google.com/drive/folders/1NQ5eMCh5COhyK51cTWIXGHaO8ziTu264", year: "2022-2023" },
+
+        { id: 2, faculty: "Sidi Bel Abess", driveLink: "https://drive.google.com/drive/folders/1JiUQswxHos8-RPCXAvOxxjtB80qqjSQx", year: "2023-2024" },
+        ]
+      },
+      {
+        id: 2, year: "2nd year", drives: [{ id: 1, faculty: "Sidi Bel Abess", driveLink: "https://drive.google.com/drive/folders/1GF_nzp1NVPPCOApvePbfaCYyn2S-PtpX", year: "2024-2025" },
+           { id: 2, faculty: "Sidi Bel Abess", driveLink: "https://drive.google.com/drive/folders/1GF_nzp1NVPPCOApvePbfaCYyn2S-PtpX", year: "2024-2025" },
+
+
+        ]
+      },
+      { id: 3, year: "3rd year", drives: [] },
+      { id: 4, year: "4th year", drives: [] },
+      { id: 5, year: "5th year", drives: [] },
+      { id: 6, year: "6th year", drives: [] }
     ]
   },
   {
     id: 3,
     name: "dental",
     years: [
-      { id: 1, year: "1st year", drives: [{ id: 1, faculty: "Blida", driveLink: "https://drive.google.com/drive/folders/1-0G8KYEe_UD7i2OKq6jIQVpSsQmELaWu", year: "2024-2025" }] },
-      { id: 2, year: "2nd year", drives: [{ id: 2, faculty: "Tlemcen", driveLink: "https://drive.google.com/drive/folders/1-0G8KYEe_UD7i2OKq6jIQVpSsQmELaWu", year: "2024-2025" }] },
-      { id: 3, year: "3rd year", drives: [{ id: 3, faculty: "Algiers", driveLink: "https://drive.google.com/drive/folders/1-0G8KYEe_UD7i2OKq6jIQVpSsQmELaWu", year: "2024-2025" }] },
-      { id: 4, year: "4th year", drives: [{ id: 4, faculty: "Oran", driveLink: "https://drive.google.com/drive/folders/1-0G8KYEe_UD7i2OKq6jIQVpSsQmELaWu", year: "2024-2025" }] },
-      { id: 5, year: "5th year", drives: [{ id: 5, faculty: "Constantine", driveLink: "https://drive.google.com/drive/folders/1-0G8KYEe_UD7i2OKq6jIQVpSsQmELaWu", year: "2024-2025" }] },
-      { id: 6, year: "6th year", drives: [{ id: 6, faculty: "Annaba", driveLink: "https://drive.google.com/drive/folders/1-0G8KYEe_UD7i2OKq6jIQVpSsQmELaWu", year: "2024-2025" }] }
+      { id: 1, year: "1st year", drives: [] },
+      { id: 2, year: "2nd year", drives: [] },
+      { id: 3, year: "3rd year", drives: [] },
+      { id: 4, year: "4th year", drives: [] },
+      { id: 5, year: "5th year", drives: [] },
+      { id: 6, year: "6th year", drives: [] }
     ]
   }
 ];
